3e21cdba7e3d10319d6a49561a771987121b289e
rsampdev <rsampdev@gmail.com>
2016-08-23 18:35:52 -0400

    Initial Commit

---
+++ Coffee Shop/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Coffee Shop
+//
+//  Created by Rodney Sampson on 8/23/16.
+//  Copyright © 2016 Rodney Sampson II. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
